f9b1a3532205069599bf2cbec0644b34add3ef00
Neal Powell <36735795+pashneal@users.noreply.github.com>
2026-07-11 09:13:10 -0400

    performance: make a couple more tweaks to single_step (#36)

--- benches/stress_test.rs
+++ benches/stress_test.rs
@@ -24,6 +24,7 @@ fn bench_stress_test_beetle(c: &mut Criterion) {
     let mut undoer = Undoer::new();
     let untimed_run = undoer.untimed_run(200, 47);
     let tracked_run = undoer.track_piece(untimed_run, Piece::new(Beetle, White));
+    undoer.warmup_gates();
 
     c.bench_function("stress_test_beetle", |b| 
         b.iter(|| undoer.beetle_run(tracked_run.clone()))
@@ -37,6 +38,7 @@ fn bench_stress_test_ant(c: &mut Criterion) {
     let mut undoer = Undoer::new();
     let untimed_run = undoer.untimed_run(200, 48);
     let tracked_run = undoer.track_piece(untimed_run, Piece::new(Ant, White));
+    undoer.warmup_gates();
 
     c.bench_function("stress_test_ant", |b| 
         b.iter(|| undoer.ant_run(tracked_run.clone()))
@@ -49,6 +51,7 @@ fn bench_stress_test_spider(c: &mut Criterion) {
     let mut undoer = Undoer::new();
     let untimed_run = undoer.untimed_run(200, 49);
     let tracked_run = undoer.track_piece(untimed_run, Piece::new(Spider, White));
+    undoer.warmup_gates();
 
     c.bench_function("stress_test_spider", |b| 
         b.iter(|| undoer.spider_run(tracked_run.clone()))
@@ -74,6 +77,7 @@ fn bench_stress_test_ladybug(c: &mut Criterion) {
     let mut undoer = Undoer::new();
     let untimed_run = undoer.untimed_run(200, 51);
     let tracked_run = undoer.track_piece(untimed_run, Piece::new(Ladybug, White));
+    undoer.warmup_gates();
 
     c.bench_function("stress_test_ladybug", |b| 
         b.iter(|| undoer.ladybug_run(tracked_run.clone()))
@@ -86,6 +90,7 @@ fn bench_stress_test_mosquito(c: &mut Criterion) {
     let mut undoer = Undoer::new();
     let untimed_run = undoer.untimed_run(200, 52);
     let tracked_run = undoer.track_piece(untimed_run, Piece::new(Mosquito, White));
+    undoer.warmup_gates();
 
     c.bench_function("stress_test_mosquito", |b| 
         b.iter(|| undoer.mosquito_run(tracked_run.clone()))
@@ -98,6 +103,7 @@ fn bench_stress_test_grasshopper(c: &mut Criterion) {
     let mut undoer = Undoer::new();
     let untimed_run = undoer.untimed_run(200, 53);
     let tracked_run = undoer.track_piece(untimed_run, Piece::new(Grasshopper, White));
+    undoer.warmup_gates();
 
     c.bench_function("stress_test_grasshopper", |b| 
         b.iter(|| undoer.grasshopper_run(tracked_run.clone()))
